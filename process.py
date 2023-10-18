66499a711cc72be838e3e633a45e1de7ff362204
ping <konpah3019@gmail.com>
2023-10-18 10:56:16 +0700

    rename variable

--- process.py
+++ process.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 def blend(img1, img2):
 
-    girl = cv2.imread(img1)
-    girl = cv2.cvtColor(girl, cv2.COLOR_BGR2RGB)
+    image1 = cv2.imread(img1)
+    image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
 
-    fuji = cv2.imread(img2)
-    fuji = cv2.cvtColor(fuji, cv2.COLOR_BGR2RGB)
+    image2 = cv2.imread(img2)
+    image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
 
-    fuji = cv2.resize(fuji, (girl.shape[1], girl.shape[0]))
+    image2 = cv2.resize(image2, (image1.shape[1], image1.shape[0]))
 
-    all_pixel = girl.shape[1] * girl.shape[0]
+    all_pixel = image1.shape[1] * image1.shape[0]
 
     color = ('b','g','r')
 
@@ -20,13 +20,13 @@ def blend(img1, img2):
     for i,col in enumerate(color):
         
         #To use OpenCV's calcHist function, uncomment below
-        histr = cv2.calcHist([girl],[i],None,[256],[0,256])
+        histr = cv2.calcHist([image1],[i],None,[256],[0,256])
         
         #To use numpy histogram function, uncomment below
         #histr, _ = np.histogram(colorimage[:,:,i],256,[0,256])
         histr = [i/all_pixel for i in histr]
 
-    girl_sum = []
+    image1_sum = []
 
     # plt.subplot(333)
 
@@ -34,43 +34,32 @@ def blend(img1, img2):
     for i,col in enumerate(color):
         
         #To use OpenCV's calcHist function, uncomment below
-        histr = cv2.calcHist([girl],[i],None,[256],[0,256])
+        histr = cv2.calcHist([image1],[i],None,[256],[0,256])
         
-        #To use numpy histogram function, uncomment below
-        #histr, _ = np.histogram(colorimage[:,:,i],256,[0,256])
         s = 0
         cdf = []
         for i in histr:
             s += i
-            girl_sum.append(int(s))
+            image1_sum.append(int(s))
             cdf.append(s/all_pixel) 
-    #     plt.plot(cdf, color=col)
-
-    # plt.title("CDF Image")
-
-    # plt.subplot(334)
-    # plt.imshow(fuji)
-    # plt.title("Original Template Image")
-
-    # plt.subplot(335)
 
     #Loop through each color sequentially
     for i,col in enumerate(color):
         
         #To use OpenCV's calcHist function, uncomment below
-        histr = cv2.calcHist([fuji],[i],None,[256],[0,256])
+        histr = cv2.calcHist([image2],[i],None,[256],[0,256])
         
         #To use numpy histogram function, uncomment below
         #histr, _ = np.histogram(colorimage[:,:,i],256,[0,256])
         histr = [i/all_pixel for i in histr]
     
-    fuji_sum = []
+    image2_sum = []
 
     #Loop through each color sequentially
     for i,col in enumerate(color):
         
         #To use OpenCV's calcHist function, uncomment below
-        histr = cv2.calcHist([fuji],[i],None,[256],[0,256])
+        histr = cv2.calcHist([image2],[i],None,[256],[0,256])
         
         #To use numpy histogram function, uncomment below
         #histr, _ = np.histogram(colorimage[:,:,i],256,[0,256])
@@ -78,7 +67,7 @@ def blend(img1, img2):
         cdf = []
         for i in histr:
             s += i
-            fuji_sum.append(int(s))
+            image2_sum.append(int(s))
             cdf.append(s/all_pixel) 
    
     def nearest_val(val,tarr):
@@ -94,37 +83,37 @@ def blend(img1, img2):
         return dum_dex
 
     #blue
-    girl_blue = girl[:,:,0].flatten()
-    fuji_blue = fuji[:,:,0].flatten()
-    for i in range(len(girl_blue)):
-        girl_blue_cdf = girl_sum[girl_blue[i]]
-        mapping = nearest_val(girl_blue_cdf, fuji_sum[0:256])
-        girl_blue[i] = mapping
-    girl_blue = np.reshape(girl_blue,(girl.shape[0],girl.shape[1]))
+    image1_blue = image1[:,:,0].flatten()
+    image2_blue = image2[:,:,0].flatten()
+    for i in range(len(image1_blue)):
+        image1_blue_cdf = image1_sum[image1_blue[i]]
+        mapping = nearest_val(image1_blue_cdf, image2_sum[0:256])
+        image1_blue[i] = mapping
+    image1_blue = np.reshape(image1_blue,(image1.shape[0],image1.shape[1]))
 
     #green
-    girl_green = girl[:,:,1].flatten()
-    fuji_green = fuji[:,:,1].flatten()
-    for i in range(len(girl_green)):
-        girl_green_cdf = girl_sum[girl_green[i]+256]
-        mapping = nearest_val(girl_green_cdf, fuji_sum[256:512])
-        girl_green[i] = mapping
-    girl_green = np.reshape(girl_green,(girl.shape[0],girl.shape[1]))
+    image1_green = image1[:,:,1].flatten()
+    image2_green = image2[:,:,1].flatten()
+    for i in range(len(image1_green)):
+        image1_green_cdf = image1_sum[image1_green[i]+256]
+        mapping = nearest_val(image1_green_cdf, image2_sum[256:512])
+        image1_green[i] = mapping
+    image1_green = np.reshape(image1_green,(image1.shape[0],image1.shape[1]))
 
     #red
-    girl_red = girl[:,:,2].flatten()
-    fuji_red = fuji[:,:,2].flatten()
-    for i in range(len(girl_red)):
-        girl_red_cdf = girl_sum[girl_red[i]+512]
-        mapping = nearest_val(girl_red_cdf, fuji_sum[512:768])
-        girl_red[i] = mapping
-    girl_red = np.reshape(girl_red,(girl.shape[0],girl.shape[1]))
+    image1_red = image1[:,:,2].flatten()
+    image2_red = image2[:,:,2].flatten()
+    for i in range(len(image1_red)):
+        image1_red_cdf = image1_sum[image1_red[i]+512]
+        mapping = nearest_val(image1_red_cdf, image2_sum[512:768])
+        image1_red[i] = mapping
+    image1_red = np.reshape(image1_red,(image1.shape[0],image1.shape[1]))
 
     #res
-    res = girl.copy()
-    res[:,:,0] = girl_blue
-    res[:,:,1] = girl_green
-    res[:,:,2] = girl_red
+    res = image1.copy()
+    res[:,:,0] = image1_blue
+    res[:,:,1] = image1_green
+    res[:,:,2] = image1_red
 
     
    
@@ -140,7 +129,7 @@ def blend(img1, img2):
 
     # plt.title("Result Image")
 
-    fuji_sum = []
+    image2_sum = []
 
     # plt.subplot(339)
 
